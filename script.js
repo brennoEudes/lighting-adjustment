cf0c653101303dae0a03a3ca6cb148d1955f62c1
“Brenno <brennoeudes@gmail.com>
2023-06-27 12:07:01 -0300

    colorSlider function

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+// Acessando os elementos
 const colorPreview = document.querySelector(".preview");
 const colorSlider = document.getElementById("hue-slider");
 const brightnessSlider = document.getElementById("brightness-slider");
 const contrastSlider = document.getElementById("contrast-slider");
+
+// Funções para cada elemento
+
+colorSlider.addEventListener("input", function () { // JS escuta o elemento no HTML!
+    const hue = this.value; 
+    const color = `hsl(${hue}, 100%, 50%)`; // hue é a cor, 100% é o contraste e 50% é o brilho!
+
+    colorPreview.style.backgroundColor = color;
+    colorPreview.style.boxShadow = `0px 0px 53px 13px ${color}`;
+
+    console.log (color, hue);
+});
