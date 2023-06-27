a1c5f1ac224424541885a8ecd130464a3cb71977
“Brenno <brennoeudes@gmail.com>
2023-06-27 11:49:53 -0300

    consts declarations

---
+++ script.js
@@ -0,0 +1,4 @@
+const colorPreview = document.querySelector(".preview");
+const colorSlider = document.getElementById("hue-slider");
+const brightnessSlider = document.getElementById("brightness-slider");
+const contrastSlider = document.getElementById("contrast-slider");
